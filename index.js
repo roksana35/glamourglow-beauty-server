ccc8325f1b91fcb276aaf9250cb8a33a5ae4592f
roksana35 <roksana7235@gmail.com>
2024-05-11 21:40:00 +0600

    get single id data from database

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express =require('express');
 const cors=require('cors');
 require('dotenv').config();
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 
 const app=express();
 const port=process.env.PORT || 5000;
@@ -32,6 +32,12 @@ async function run() {
     app.get('/services',async(req,res)=>{
       const cursor=serviceCollection.find();
       const result=await cursor.toArray();
+      res.send(result)
+    })
+
+    app.get('/services/:id',async(req,res)=>{
+      console.log(req.params.id)
+      const result=await serviceCollection.findOne({_id:new ObjectId(req.params.id)})
       res.send(result)
     })
 
