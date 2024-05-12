b36356343f1ac83c9aa36465deef5250a3713958
roksana35 <roksana7235@gmail.com>
2024-05-12 20:49:30 +0600

    deploy vercel

--- index.js
+++ index.js
@@ -6,7 +6,16 @@ const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const app=express();
 const port=process.env.PORT || 5000;
 
-app.use(cors());
+app.use(
+  cors({
+    origin: [
+      "http://localhost:5173",
+      "https://beauty-services-935c4.web.app",
+      "https://beauty-services-935c4.firebaseapp.com",
+    ],
+    credentials: true,
+  })
+);
 app.use(express.json());
 
 
@@ -25,7 +34,7 @@ const client = new MongoClient(uri, {
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
+    // await client.connect();
 
     const serviceCollection=client.db('beautyDb').collection('services')
     const purchaseCollection=client.db('beautyDb').collection('purchase')
@@ -85,7 +94,7 @@ async function run() {
         res.send(result)
     })
     // Send a ping to confirm a successful connection
-    await client.db("admin").command({ ping: 1 });
+    // await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
     // Ensures that the client will close when you finish/error
